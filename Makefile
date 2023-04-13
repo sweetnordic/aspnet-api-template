dbb93244d68ec3592086324a4455651454b10fdc
Sven Hempke <ich@nor-dic.de>
2023-04-13 22:31:55 +0200

    initial dotnet code

---
+++ Makefile
@@ -0,0 +1,2 @@
+container:
+	cd .docker && docker compose up --quiet-pull --build --detach --force-recreate
---
+++ src/Host/Program.cs
@@ -0,0 +1,24 @@
+namespace Company.Application1.Host;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
